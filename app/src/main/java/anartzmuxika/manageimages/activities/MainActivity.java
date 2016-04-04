d36b41c9fe60d1060d7fd71802f27ec9e0e9636a
Anartz Mugika <mugan86@gmail.com>
2016-04-04 09:20:06 +0200

    Store select photo in Preferences File to use after when load from preferences

--- app/src/main/java/anartzmuxika/manageimages/activities/MainActivity.java
+++ app/src/main/java/anartzmuxika/manageimages/activities/MainActivity.java
@@ -27,6 +27,7 @@ import java.io.FileDescriptor;
 
 import anartzmuxika.manageimages.R;
 import anartzmuxika.manageimages.utils.ConstantValues;
+import anartzmuxika.manageimages.utils.DataPreferences;
 import anartzmuxika.manageimages.utils.Directory;
 
 public class MainActivity extends AppCompatActivity {
@@ -89,51 +90,39 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int item) {
 
                         //Open camera to capture image
-                        if (options[item].equals("Camera"))
-                        {
+                        if (options[item].equals("Camera")) {
                             if (checkSelfPermission(Manifest.permission.CAMERA)
-                                    != PackageManager.PERMISSION_GRANTED && Build.VERSION.SDK_INT>=23) {
+                                    != PackageManager.PERMISSION_GRANTED && Build.VERSION.SDK_INT >= 23) {
 
                                 requestPermissions(new String[]{Manifest.permission.CAMERA},
                                         ConstantValues.GRANTED_CAMERA);
-                            }
-                            else
-                            {
+                            } else {
                                 openCamera();
                             }
 
-                        }
-                        else if (options[item].equals("Open Storage file reference"))
-                        {
-                            //Change reference after take one reference from image picker or camera
-                            //Necessary add "android.permission.READ_EXTERNAL_STORAGE" permission in manifest and accept permission in version Android M
-                            bitmap = BitmapFactory.decodeFile("/storage/emulated/0/DCIM/Camera/IMG_20151114_185554.jpg");
-                            show_loadImageView.setImageBitmap(bitmap);
+                        } else if (options[item].equals("Open Storage file reference")) {
+
+                            if (!DataPreferences.getPreference(getApplicationContext(), "Local_Reference").equals("")) {
+                                //Change reference after take one reference from image picker or camera
+                                //Necessary add "android.permission.READ_EXTERNAL_STORAGE" permission in manifest and accept permission in version Android M
+                                bitmap = BitmapFactory.decodeFile(DataPreferences.getPreference(getApplicationContext(), "Local_Reference"));
+
+                                show_loadImageView.setImageBitmap(bitmap);
+                            }
+                            else
+                            {
+                                openDeviceGallery();
+                            }
                         }
 
                         //Get image from gallery
                         else if (options[item].equals("Device (Gallery)"))
-
                         {
-                            if (Build.VERSION.SDK_INT < 19){
-                                Intent intent = new   Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                                startActivityForResult(Intent.createChooser(intent, "Complete action using"), ConstantValues.IMAGE_PICKER_SELECT);
-                            }
-                            else
-                            {
-                                Intent intent = new Intent(Intent.ACTION_PICK,
-                                        MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-                                File dir=
-                                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
-                                output=new File(dir, "image.jpeg");
-                                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(output));
-                                startActivityForResult(intent, ConstantValues.IMAGE_PICKER_SELECT);
-                            }
+                            openDeviceGallery();
                         }
 
                         //Exit dialog
-                        else if (options[item].equals("Cancel"))
-                        {
+                        else if (options[item].equals("Cancel")) {
                             dialog.dismiss();
                         }
                     }
@@ -151,6 +140,22 @@ public class MainActivity extends AppCompatActivity {
         output=new File(dir, "image.jpeg");
         i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(output));
         startActivityForResult(i, ConstantValues.IMAGE_PICKER_CAMERA);
+    }
+
+    private void openDeviceGallery()
+    {
+        if (Build.VERSION.SDK_INT < 19) {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(Intent.createChooser(intent, "Complete action using"), ConstantValues.IMAGE_PICKER_SELECT);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_PICK,
+                    MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+            File dir =
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+            output = new File(dir, "image.jpeg");
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(output));
+            startActivityForResult(intent, ConstantValues.IMAGE_PICKER_SELECT);
+        }
     }
 
     @Override
@@ -246,6 +251,13 @@ public class MainActivity extends AppCompatActivity {
                         //Toast.makeText(getApplicationContext(), "URL: " + imageUri, Toast.LENGTH_LONG).show();
                         System.out.println("URL (After convert): " + picturePath);
                         //Toast.makeText(getApplicationContext(), "URL (After convert): " + picturePath, Toast.LENGTH_LONG).show();
+
+
+
+                        String [] propertyName = {"Local_Reference"};
+                        String [] propertyValue = {picturePath};
+
+                        DataPreferences.setPreference(getApplicationContext(), propertyName, propertyValue);
 
                         if (imageUri.toString().contains("content://media/"))
                         {
