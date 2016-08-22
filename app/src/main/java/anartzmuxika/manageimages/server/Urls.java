eace3d3b2056846c188fa3dbac8e5d87d24d8403
Anartz Mugika <mugan86@gmail.com>
2016-08-22 12:53:30 +0200

    Add upload file server url

---
+++ app/src/main/java/anartzmuxika/manageimages/server/Urls.java
@@ -0,0 +1,10 @@
+package anartzmuxika.manageimages.server;
+
+/**
+ * Created by anartzmugika on 22/8/16.
+ */
+
+public class Urls {
+
+    public static final String URL_LOCALHOST = "http://mugan86.com/android_images/UploadToServer.php";
+}
