6a79ab0745ecc67611bb53bf26d476bc18995a6b
anartzmugika <mugan86@gmail.com>
2016-12-02 21:03:32 +0100

    Add Local url

--- app/src/main/java/anartzmuxika/manageimages/server/Urls.java
+++ app/src/main/java/anartzmuxika/manageimages/server/Urls.java
@@ -1,10 +1,11 @@
 package anartzmuxika.manageimages.server;
 
-/**
+/*******************************************************
  * Created by anartzmugika on 22/8/16.
  */
 
 public class Urls {
 
     public static final String URL_LOCALHOST = "http://mugan86.com/android_images/UploadToServer.php";
+    public static final String URL_LOCALHOST_LOCAL = "http://192.168.1.5/uploader/upload_file.php";
 }
