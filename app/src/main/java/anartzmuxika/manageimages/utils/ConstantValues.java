26ad25dba034e38789655e63dffe424a62216fd2
anartzmugika <mugan86@gmail.com>
2016-12-02 21:05:11 +0100

    Add new constants

--- app/src/main/java/anartzmuxika/manageimages/utils/ConstantValues.java
+++ app/src/main/java/anartzmuxika/manageimages/utils/ConstantValues.java
@@ -1,6 +1,6 @@
 package anartzmuxika.manageimages.utils;
 
-/**
+/**************************************
  * Created by anartzmugika on 23/3/16.
  */
 public class ConstantValues {
@@ -14,4 +14,11 @@ public class ConstantValues {
 
     public static final int GRANTED_CAMERA = 10;
     public static final int GRANTED_ACCESS_STORAGE = 11;
+
+    public static final int MAX_WIDTH_HEIGHT_IMG = 1024;
+
+    //Directory constants
+
+    public static final String CONTENT_PROVIDER = "content://com.google.android.apps.photos.contentprovider";
+    public static final String CONTENT_MEDIA = "content://media/";
 }
