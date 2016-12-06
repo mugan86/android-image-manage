73c8f2ad84bbb4fde80dac5fff52745c53618e2d
Anartz Muxika Ledo <mugan86@gmail.com>
2016-12-06 10:07:15 +0100

    Add localhost

--- app/src/main/java/anartzmuxika/manageimages/server/Urls.java
+++ app/src/main/java/anartzmuxika/manageimages/server/Urls.java
@@ -7,5 +7,6 @@ package anartzmuxika.manageimages.server;
 public class Urls {
 
     //public static final String URL_LOCALHOST = "http://mugan86.com/android_images/upload_file.php";
-    public static final String URL_LOCALHOST_LOCAL = "http://192.168.1.5/uploader/upload_file.php";
+    //public static final String URL_LOCALHOST_LOCAL = "http://192.168.1.5/uploader/upload_file.php";
+    public static final String URL_LOCALHOST_LOCAL = "http://192.168.110.131/uploader/upload_file.php";
 }
