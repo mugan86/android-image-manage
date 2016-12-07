c15a7d4bafc0874b55cf95300c98abedd71c6ec0
Anartz Muxika Ledo <mugan86@gmail.com>
2016-12-07 08:37:03 +0100

    Add references in FileUploadListener

--- app/src/main/java/anartzmuxika/manageimages/FileUploadListener.java
+++ app/src/main/java/anartzmuxika/manageimages/FileUploadListener.java
@@ -1,12 +1,21 @@
 package anartzmuxika.manageimages;
 
-/**
- * Created by anartzmugika on 22/8/16.
+/**************************************************************************************************
+ * Created by anartzmugika on 7/12/2016.
  */
 
 public interface FileUploadListener {
+    /**
+     *
+     * @param percentage: Progress number to 100%
+     * @param kb: Total of size to file
+     */
     void onUpdateProgress(int percentage, long kb);
 
+    /**
+     *
+     * @return cancelled or no
+     */
     boolean isCanceled();
 
     void transferred(long num, long max);
