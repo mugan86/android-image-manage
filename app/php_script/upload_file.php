ad8f85716eddac76730a340c1e6c9687e1ebff8f
Anartz Muxika Ledo <mugan86@gmail.com>
2016-12-07 08:38:23 +0100

    Update script to return only json result

--- app/php_script/upload_file.php
+++ app/php_script/upload_file.php
@@ -1,6 +1,6 @@
 <?php
 
-	echo "Image upload datetime: " . date("Y-m-d h:i:s") . '<br/>';
+	//echo "Image upload datetime: " . date("Y-m-d h:i:s") . '<br/>';
 
 	$file = $_FILES['uploaded_file'];
 	$name = $file['name'];
@@ -9,11 +9,11 @@
 	$extension = explode('.', $name);
 	$extension = strtolower((end($extension)));
 
-	echo "EXTENSION: " . $extension . '<br/>';
+	//echo "EXTENSION: " . $extension . '<br/>';
 
 	if ($extension != "png" && $extension != "jpg" && $extension != "jpeg")
 	{
-		echo "Not exist extension and add png extension";
+		//echo "Not exist extension and add png extension";
 		$extension = "png";
 	}
 
@@ -23,7 +23,8 @@
 	$tmp_file_name = "servirace_app{$name}.{$extension}";
 	$tmp_file_path = "uploaded_files/" . $tmp_file_name;
 
-	echo $tmp_file_path . '<br/>';
+
+	//echo $tmp_file_path . '<br/>';
 
 
 	//Name of file with datetime
@@ -34,8 +35,14 @@
 
 
 	if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $tmp_file_path)) {
-	    echo "File ". $target_path . " upload succesfully. Thank you " . $name . " !!! Filename: " . $filename;
+	    //echo "File ". $target_path . " upload succesfully. Thank you " . $name . " !!! Filename: " . $filename;
+	    $row_array['message'] = "File ". $tmp_file_path . " upload succesfully. Thank you " . $name . " !!! Filename: " . $filename;
+		$row_array['success'] = true;
+		echo json_encode($row_array);
 	} else{
-	    echo "Error in file upload";
+	    //echo "Error in file upload";
+	    $row_array['message'] = "Error in file upload. Try again please.";
+		$row_array['success'] = false;
+		echo json_encode($row_array);
 	}
 ?>
