ec2d2b9864476f0a3060af8a8fe12f81cd72c3f2
Anartz Muxika Ledo <mugan86@gmail.com>
2016-12-12 13:05:06 +0100

    Add languages manage options

--- app/src/main/java/anartzmuxika/manageimages/utils/DataPreferences.java
+++ app/src/main/java/anartzmuxika/manageimages/utils/DataPreferences.java
@@ -1,8 +1,11 @@
 package anartzmuxika.manageimages.utils;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+
+import java.util.Locale;
 
 /**
  * Created by anartzmugika on 4/4/16.
@@ -19,6 +22,46 @@ import android.preference.PreferenceManager;
  * DataPreferences.setPreference(getApplicationContext(), propertyName, propertyValue);
  */
 public class DataPreferences {
+
+    //Default constants values
+    private static final String DEFAULT_PREFERENCES = "Default Preferences";
+    private static final String LANG_PROPERTY = "Language";
+    private static final String DEFAULT_LANG = "en";
+
+    public static String getLocaleLanguage (Context context)
+    {
+        String langPref = LANG_PROPERTY;
+        SharedPreferences prefs = context.getSharedPreferences(DEFAULT_PREFERENCES, Activity.MODE_PRIVATE);
+        return prefs.getString(langPref, DEFAULT_LANG);
+    }
+    public static void loadLocale(Context context)
+    {
+        String langPref = LANG_PROPERTY;
+        SharedPreferences prefs = context.getSharedPreferences(DEFAULT_PREFERENCES, Activity.MODE_PRIVATE);
+        String language = prefs.getString(langPref, DEFAULT_LANG);
+        changeLang(language, context);
+    }
+
+    public static void changeLang(String lang, Context context)
+    {
+        if (lang.equalsIgnoreCase(""))
+            return;
+        Locale myLocale = new Locale(lang);
+        saveLocale(lang, context);
+        Locale.setDefault(myLocale);
+        android.content.res.Configuration config = new android.content.res.Configuration();
+        config.locale = myLocale;
+        context.getResources().updateConfiguration(config, context.getResources().getDisplayMetrics());
+    }
+
+    private static void saveLocale(String lang, Context context)
+    {
+        String langPref = LANG_PROPERTY;
+        SharedPreferences prefs = context.getSharedPreferences(DEFAULT_PREFERENCES, Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(langPref, lang);
+        editor.commit();
+    }
     public static SharedPreferences getPreferencesFile(Context context) {
         // This sample app persists the registration ID in shared preferences, but
         // how you store the regID in your app is up to you.
