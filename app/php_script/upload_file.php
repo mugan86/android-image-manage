d4deb4f7deaf3368ffa75c87b14a128028096dcb
anartzmugika <mugan86@gmail.com>
2016-12-22 19:59:26 +0100

    Upload file with receive name from app

--- app/php_script/upload_file.php
+++ app/php_script/upload_file.php
@@ -1,42 +1,29 @@
 <?php
-
 	//echo "Image upload datetime: " . date("Y-m-d h:i:s") . '<br/>';
-
 	$file = $_FILES['uploaded_file'];
 	$name = $file['name'];
 	$tmp_name = $file['tmp_name'];
-
+	$filename_1 = $_POST['filename'];
 	$extension = explode('.', $name);
 	$extension = strtolower((end($extension)));
-
 	//echo "EXTENSION: " . $extension . '<br/>';
-
-	if ($extension != "png" && $extension != "jpg" && $extension != "jpeg")
+	if ($extension == "png" && $extension == "jpg")
 	{
 		//echo "Not exist extension and add png extension";
-		$extension = "png";
+		$extension = "jpeg";
 	}
-
 	//$title = $_POST['title'] ? $_POST['title'] : "Servirace";
-
 	//New file name and new path to upload to server
 	$tmp_file_name = "servirace_app{$name}.{$extension}";
-	$tmp_file_path = "uploaded_files/" . $tmp_file_name;
-
-
+	$tmp_file_path = "uploaded_files/" . trim($filename_1);
 	//echo $tmp_file_path . '<br/>';
-
-
 	//Name of file with datetime
 	$filename = $name . "_" . date("Y_m_d_h_i_s");
-
 	//Not use, default target path (NOT DELETE to TEST!!)
 	$target_path = "uploaded_files/" . basename( $_FILES['uploaded_file']['name']);
-
-
 	if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $tmp_file_path)) {
 	    //echo "File ". $target_path . " upload succesfully. Thank you " . $name . " !!! Filename: " . $filename;
-	    $row_array['message'] = "File ". $tmp_file_path . " upload succesfully. Thank you " . $name . " !!! Filename: " . $filename;
+	    $row_array['message'] = "File ". $tmp_file_path . " upload succesfully. Thank you " . $name . " !!! Filename: " . $filename . " // " . $filename_1;
 		$row_array['success'] = true;
 		echo json_encode($row_array);
 	} else{
