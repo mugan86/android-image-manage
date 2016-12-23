748e7cf033046117bdb84dcd51aeba4332a4b7dd
anartzmugika <mugan86@gmail.com>
2016-12-23 20:31:30 +0100

    Manage activity with new button

--- app/src/main/java/anartzmuxika/manageimages/activities/MainActivity.java
+++ app/src/main/java/anartzmuxika/manageimages/activities/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import anartzmuxika.manageimages.R;
 import anartzmuxika.manageimages.utils.DataPreferences;
@@ -28,10 +29,24 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                Intent open_upload_activity_intent = new Intent(MainActivity.this, UploadManagerActivity.class);
-                startActivity(open_upload_activity_intent);
+                openImageManageOptionsActivity();
+
             }
         });
+
+        Button open_image_manage_optionsButton = (Button) findViewById(R.id.open_image_manage_optionsButton);
+        open_image_manage_optionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImageManageOptionsActivity();
+            }
+        });
+    }
+
+    private void openImageManageOptionsActivity()
+    {
+        Intent open_upload_activity_intent = new Intent(MainActivity.this, UploadManagerActivity.class);
+        startActivity(open_upload_activity_intent);
     }
 
 }
