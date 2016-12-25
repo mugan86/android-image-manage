7830457d687b05c0cbcf3a79d6fc218db1778197
anartzmugika <mugan86@gmail.com>
2016-12-25 10:44:19 +0100

    Manage filename with user basic info

---
+++ app/src/main/java/anartzmuxika/manageimages/utils/Filename.java
@@ -0,0 +1,14 @@
+package anartzmuxika.manageimages.utils;
+
+/**********************************************************************
+ * Created by anartzmugika on 25/12/16.
+ */
+
+public class Filename {
+
+    public String getFIlenameWithNameBasicData(String name_basic_data, String filename)
+    {
+        if (!name_basic_data.equals("")) name_basic_data = name_basic_data.replace(" ", "_").toLowerCase();
+        return String.format(filename, name_basic_data);
+    }
+}
