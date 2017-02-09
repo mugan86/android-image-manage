214e505edbc77e879e1fec695bddb07120672a42
anartzmugika <mugan86@gmail.com>
2017-02-09 20:22:45 +0100

    Add servirace url

--- app/src/main/java/anartzmuxika/manageimages/server/Urls.java
+++ app/src/main/java/anartzmuxika/manageimages/server/Urls.java
@@ -6,7 +6,7 @@ package anartzmuxika.manageimages.server;
 
 public class Urls {
 
-    public static final String URL_LOCALHOST = "http://www.mugan86.com/android_images/upload_file.php";
+    public static final String URL_LOCALHOST = "https://mugan86.com/serviraces/api/v2/photo/upload_file.php";
     public static final String URL_LOCALHOST_LOCAL = "http://192.168.1.4/uploader/upload_file.php";
     //public static final String URL_LOCALHOST_LOCAL = "http://192.168.110.131/uploader/upload_file.php";
 }
