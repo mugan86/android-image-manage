909f7daa6b3fe84b5648a0fe8f5973477e2f216e
anartzmugika <mugan86@gmail.com>
2017-02-10 19:36:57 +0100

    Update MultiPartUtility to manage File (in this case photo) before upload to server with progress

---
+++ app/src/main/java/anartzmuxika/manageimages/server/MultipartUtility.java
@@ -0,0 +1,190 @@
+package anartzmuxika.manageimages.server;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import anartzmuxika.manageimages.FileUploadListener;
+import anartzmuxika.manageimages.utils.DateTime;
+
+/************************************************************
+ * Created by anartzmugika on 10/2/17.
+ * Manage Image File to upload with select name (Pending to optimize
+ */
+
+class MultipartUtility
+{
+    private FileUploadListener listener;
+    private Context context;
+    public MultipartUtility(FileUploadListener listener, Context context)
+    {
+        this.listener = listener;
+        this.context = context;
+    }
+
+    public int uploadFile(final String selectedFilePath, String url_, File file) {
+
+        //Send filename (use in server with ['uploaded_file'] ['filename']
+        String filename = DateTime.getCurrentDataTime(false);
+        int serverResponseCode = 0;
+
+        HttpURLConnection connection;
+        DataOutputStream dataOutputStream;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+
+
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        File selectedFile = new File(selectedFilePath);
+
+
+        String[] parts = selectedFilePath.split("/");
+        final String fileName = parts[parts.length - 1];
+
+        if (!selectedFile.isFile()) {
+            //dialog.dismiss();
+
+            ((Activity)context).runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Source File Doesn't Exist: " + selectedFilePath);
+                }
+            });
+            return 0;
+        } else {
+            try {
+                FileInputStream fileInputStream = new FileInputStream(selectedFile);
+                URL url = new URL(url_);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);//Allow Inputs
+                connection.setDoOutput(true);//Allow Outputs
+                connection.setUseCaches(false);//Don't use a cached Copy
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Connection", "Keep-Alive");
+                connection.setRequestProperty("ENCTYPE", "multipart/form-data");
+                connection.setRequestProperty(
+                        "Content-Type", "multipart/form-data;boundary=" + boundary);
+                connection.setRequestProperty("uploaded_file",file.getAbsolutePath());
+
+
+                //creating new dataoutputstream
+                dataOutputStream = new DataOutputStream(connection.getOutputStream());
+
+                //writing bytes to data outputstream
+                dataOutputStream.writeBytes(twoHyphens + boundary + lineEnd);
+                dataOutputStream.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
+                        + filename + "\"" + lineEnd);
+
+                dataOutputStream.writeBytes(lineEnd);
+
+                //returns no. of bytes present in fileInputStream
+                bytesAvailable = fileInputStream.available();
+                //selecting the buffer size as minimum of available bytes or 1 MB
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                //setting the buffer as byte array of size of bufferSize
+                buffer = new byte[bufferSize];
+
+                //reads bytes from FileInputStream(from 0th index of buffer to buffersize)
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                long totalSize = selectedFile.length();
+                long totalRead = 0;
+                //loop repeats till bytesRead = -1, i.e., no bytes are left to read
+
+                //TODO add fileuploadlistener
+                while (bytesRead > 0) {
+
+                    try {
+
+                        //write the bytes read from inputstream
+                        dataOutputStream.write(buffer, 0, bufferSize);
+
+                        totalRead += bytesRead;
+                        int percentage = (int) ((totalRead / (float) totalSize) * 100);
+                        //outputStream.write(buffer, 0, bytesRead);
+
+                        long now = System.currentTimeMillis();
+
+                        Log.e("", totalRead + " " + " " + percentage);
+                        this.listener.onUpdateProgress(percentage, totalRead);
+                    } catch (OutOfMemoryError e) {
+                        Toast.makeText(context, "Insufficient Memory!", Toast.LENGTH_SHORT).show();
+                    }
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                }
+
+                dataOutputStream.writeBytes(lineEnd);
+                dataOutputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                try{
+                    serverResponseCode = connection.getResponseCode();
+                }catch (OutOfMemoryError e){
+                    Toast.makeText(context, "Memory Insufficient!", Toast.LENGTH_SHORT).show();
+                }
+                String serverResponseMessage = connection.getResponseMessage();
+
+                Log.i("jjjjj", "Server Response is: " + serverResponseMessage + ": " + serverResponseCode);
+
+                //response code of 200 indicates the server status OK
+                if (serverResponseCode == 200) {
+                    ((Activity)context).runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            System.out.println("File Upload completed.\n\n You can see the uploaded file here: \n\n" + "http://coderefer.com/extras/uploads/" + fileName);
+                        }
+                    });
+                }
+
+                //closing the input and output streams
+                fileInputStream.close();
+                dataOutputStream.flush();
+                dataOutputStream.close();
+
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                ((Activity)context).runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(context, "File Not Found", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                ((Activity)context).runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(context, "URL Error!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                ((Activity)context).runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(context, "Cannot Read/Write File", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+            //dialog.dismiss();
+            return serverResponseCode;
+        }
+
+    }
+}
---
+++ app/src/main/java/anartzmuxika/manageimages/server/UploadPhotoTask.java
@@ -0,0 +1,108 @@
+package anartzmuxika.manageimages.server;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.File;
+
+import anartzmuxika.manageimages.FileUploadListener;
+
+/**
+ * Created by anartzmugika on 10/2/17.
+ */
+
+public class UploadPhotoTask extends AsyncTask<String, Integer, Integer> {
+
+    private Exception exception;
+    private ProgressDialog progressDialog;
+    private Context context;
+    private File file;
+    private String url_to_upload;
+    private String selectedImagePath;
+
+    public UploadPhotoTask(Context context, File file, String url_to_upload, String selectedImagePath)
+    {
+        this.context = context;
+        this.file = file;
+        this.url_to_upload = url_to_upload;
+        this.selectedImagePath = selectedImagePath;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        this.progressDialog =new ProgressDialog(context);
+        this.progressDialog.setMessage("Upload image...");
+        this.progressDialog.setCancelable(false);
+        this.progressDialog.setMax(100);
+        this.progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        this.progressDialog.setProgress(0);
+        this.progressDialog.show();
+    }
+
+    protected Integer doInBackground(String... urls) {
+
+        try {
+            MultipartUtility upload = new MultipartUtility(new FileUploadListener() {
+                /**
+                 * @param percentage: Progress number to 100%
+                 * @param kb:         Total of size to file
+                 */
+                @Override
+                public void onUpdateProgress(final int percentage, final long kb) {
+                    ((Activity)context).runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            //  your progress code
+                            System.out.println("% " + percentage + " / " + kb + "(size)");
+                            //progressDialog.setMessage(context.getResources().getString(R.string.download_image_data_progress) + " (" + percentage + " %)");
+                            progressDialog.setProgress(percentage);
+
+                        }});
+                }
+
+                @Override
+                public boolean isCanceled() {
+                    return false;
+                }
+
+                @Override
+                public void transferred(long num, long max) {
+
+                }
+            }, context);
+            return upload.uploadFile(selectedImagePath, url_to_upload, file);
+        } catch (Exception e) {
+            this.exception = e;
+
+            return null;
+        }
+    }
+
+    @Override
+    public void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+
+        // UPDATE THE PROGRESS DIALOG
+        System.out.println("PROGRESS----------->: " + values[0]);
+        progressDialog.setMessage(String.valueOf(" (" + values[0] + " %)"));
+    }
+
+    protected void onPostExecute(Integer int_) {
+        // TODO: check this.exception
+        // TODO: do something with the feed
+        System.out.println("SERVER RETURN: " + int_);
+        try {
+            this.progressDialog.setMessage("Finish!!");
+            Thread.sleep(1000);
+            this.progressDialog.dismiss();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
