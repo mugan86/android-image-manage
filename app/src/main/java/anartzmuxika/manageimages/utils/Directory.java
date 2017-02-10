a29fb2e46a8ca95b308c656eb6c6789777cb468f
anartzmugika <mugan86@gmail.com>
2017-02-10 19:57:20 +0100

    Rotate Image Bitmap only 90º

--- app/src/main/java/anartzmuxika/manageimages/utils/Directory.java
+++ app/src/main/java/anartzmuxika/manageimages/utils/Directory.java
@@ -317,4 +317,11 @@ public class Directory {
 
 
     }
+
+    public static Bitmap setRotateBitmap(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
 }
